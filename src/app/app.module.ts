0aabcb8c25804c24299f4844da77d961b3218859
heshanu <heshanu97@gmail.com>
2023-12-29 23:45:01 +0530

    update master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './module/material/material.module';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { PlaylistComponent } from './pages/playlist/playlist.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SongCardComponent } from './compoents/song-card/song-card.component';
+import { TopNavComponent } from './compoents/top-nav/top-nav.component';
+import { ButtonComponent } from './compoents/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    PlaylistComponent,
+    SignupComponent,
+    LoginComponent,
+    SongCardComponent,
+    TopNavComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [
     provideClientHydration()
