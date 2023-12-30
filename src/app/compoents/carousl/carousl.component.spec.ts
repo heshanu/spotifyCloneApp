e6f4a2dd1ec5969b47764e3cdf48bdb1b7e3d38e
heshanu <heshanu97@gmail.com>
2023-12-30 09:05:28 +0530

    implmented songcard

---
+++ src/app/compoents/carousl/carousl.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CarouslComponent } from './carousl.component';
+
+describe('CarouslComponent', () => {
+  let component: CarouslComponent;
+  let fixture: ComponentFixture<CarouslComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CarouslComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(CarouslComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/compoents/carousl/carousl.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carousl',
+  templateUrl: './carousl.component.html',
+  styleUrl: './carousl.component.css'
+})
+export class CarouslComponent {
+
+}
