87f7b47f0bb60a1a0776c767c149bca18b45fe68
heshanu <heshanu97@gmail.com>
2024-01-01 01:11:08 +0530

    dfd

--- src/app/compoents/song-card/song-card.component.ts
+++ src/app/compoents/song-card/song-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SongService } from '../../service/song.service';
-
+import { SearchbarService } from '../../service/searchbar.service';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'app-song-card',
   templateUrl: './song-card.component.html',
@@ -9,11 +11,27 @@ import { SongService } from '../../service/song.service';
 export class SongCardComponent implements OnInit{
 
   songListCard: any[] = [];
-  constructor(private songSerivce: SongService) { }
+
+  constructor(private searchService: SearchbarService,
+    private songService: SongService, private actRouter: ActivatedRoute) { }
+    // public songId: BehaviorSubject<any> = new BehaviorSubject<any>('');
+  selectedSong:any;
+  songName!: string;
+  songId!: any;
 
   ngOnInit(): void {
-    this.songListCard = this.songSerivce.songCards;
+    this.songListCard = this.songService.songCards;
+    this.songId = this.actRouter.snapshot.params['id'];
+    this.selectedSong = this.songService.songCards.filter((v: any) => {
+      return v.song_id === this.songId;
+    });
+    // this.searchService.songName.subscribe((value) => {
+    //   this.songId= value;
+    
+    //   this.selectedSong = this.songService.songCards.filter((v: any) => {
+    //     return v.song_id === this.songId;
+    //   });
+    // });
+
   }
- 
- 
 }
